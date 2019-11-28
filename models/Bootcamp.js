709ed58088c33f6441b23410bb3bed4a8d508c71
Kha Vo <kha.vo@orientsoftware.com>
2019-11-28 16:53:17 +0700

    add slug bootscamschema

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema(
     {
@@ -100,5 +101,9 @@ const BootcampSchema = new mongoose.Schema(
     }
 );
 
+BootcampSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
